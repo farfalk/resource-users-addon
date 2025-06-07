2736c1f239f5f52ee1d71d00ea4b7a9181c05434
farfalk <talevi.luca@gmail.com>
2025-06-07 15:51:10 +0200

    _get_all_resource_files, a little bit faster

--- addons/resource-users-addon/users_counter.gd
+++ addons/resource-users-addon/users_counter.gd
@@ -15,6 +15,7 @@ var _latest_path: String = ""
 func _init():
 	add_child(property_control)
 	_refresh_control_text()
+	property_control.modulate = Color.WHITE
 	read_only = true
 
 
@@ -54,9 +55,10 @@ func _update_property():
 		nested_users += as_text.count(resource_id)-1  # -1 because it ignores the resource definition
 	else:
 		var resource_id = ResourceUID.id_to_text(ResourceLoader.get_resource_uid(resource_path))
-		var all_tscns = _get_all_files("res://", '.tscn')
-		var all_tres = _get_all_files("res://", '.tres')
-		var all_files = all_tscns + all_tres
+		#var all_tscns = _get_all_files("res://", '.tscn')
+		#var all_tres = _get_all_files("res://", '.tres')
+		#var all_files = all_tscns + all_tres
+		var all_files = _get_all_resource_files("res://")
 		for file_path in all_files:
 			var file = FileAccess.open(file_path, FileAccess.READ)
 			var as_text = file.get_as_text()
@@ -66,7 +68,7 @@ func _update_property():
 			var split_text = as_text.substr(resource_loading_step_index, next_square_bracket_subindex).split("id=")
 			if len(split_text)>1:
 				var resource_local_id = split_text[1].lstrip("\"").rstrip("\"")
-				count += as_text.count(resource_id)
+				count += as_text.count(resource_local_id)
 			if count>0:
 				_log("found {0} occurrences in {1}".format([count, file_path]))
 				if file_path==get_tree().edited_scene_root.scene_file_path:
@@ -81,7 +83,7 @@ func _update_property():
 	var totals = "{0}".format([total_users])
 	var lp = "(" if (internal_users>0) else ""
 	var locals = "{0} in current".format([current_scene_users]) if current_scene_users>0 else ""
-	var c = ", " if (internal_users>0) else ""
+	var c = ", " if (current_scene_users>0) else ""
 	var nested = "{0} nested".format([nested_users]) if nested_users>0 else ""
 	var rp = ")" if (internal_users>0) else ""
 	if total_users>0:
@@ -101,31 +103,20 @@ func _refresh_control_text():
 	property_control.text = current_value
 
 
-## https://gist.github.com/hiulit/772b8784436898fd7f942750ad99e33e?permalink_comment_id=5196503#gistcomment-5196503
-## (with some personal edits)
-func _get_all_files(path: String, file_ext := "", files : Array[String] = []) -> Array[String]:
-	var dir : = DirAccess.open(path)
-	if file_ext.begins_with("."): # get rid of starting dot if we used, for example ".tscn" instead of "tscn"
-		file_ext = file_ext.substr(1,file_ext.length()-1)
-	if DirAccess.get_open_error() == OK:
-		dir.list_dir_begin()
-		var file_name = dir.get_next()
-		while file_name != "":
-			if dir.current_is_dir():
-				# recursion
-				files = _get_all_files(dir.get_current_dir() +"/"+ file_name, file_ext, files)
-			else:
-				if file_ext and file_name.get_extension() != file_ext:
-					file_name = dir.get_next()
-					continue
-				if dir.get_current_dir() == "res://":
-					files.append(dir.get_current_dir() + file_name)
-				else:
-					files.append(dir.get_current_dir() +"/"+ file_name)
-			file_name = dir.get_next()
-	else:
-		print("[_get_all_files()] An error occurred when trying to access %s." % path)
-	return files
+func _get_all_resource_files(path):
+	var resources = Array(ResourceLoader.list_directory(path))
+	var dir = DirAccess.open(path)
+	if dir:
+		for d in dir.get_directories():
+			resources += _get_all_resource_files(d)
+	var len = len(resources)
+	for i in range(1, len+1):
+		var res = resources[len-i]
+		if res.begins_with(".") || res.ends_with("/"):
+			resources.remove_at(len-i)
+		else:
+			resources[len-i] = path+"/"+res
+	return resources
 
 
 func _log(log_string: String):
